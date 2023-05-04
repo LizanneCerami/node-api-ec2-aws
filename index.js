08e5aa08be3ce14de4c13c216111f1e0ed29540a
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-04 15:58:05 -0400

    added list of dogs

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/',(req, res) => res.send('Welcome to my API'))
+const dogs = [
+  {
+  name: 'Paco',
+  breed: 'Chihuahua',
+  age: 6,
+},
+  {
+  name: 'Bubba',
+  breed: 'Labrador',
+  age: 10,
+},
+  {
+  name: 'Lucia',
+  breed: 'Yorkie',
+  age: 3,
+},
+  {
+  name: 'Bob',
+  breed: 'Pitbull',
+  age: 11,
+},
+]
+
+app.get('/',(req, res) => res.send(dogs))
 
 app.listen(4000, () => console.log('API running 😎 on port 4000'))
